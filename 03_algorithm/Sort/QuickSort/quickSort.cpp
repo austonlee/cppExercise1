f6d48468f46c40e7a15a8f69df9bc246ae7ae41c
AustonLee <auston.cc.lee@mail.foxconn.com>
2023-04-18 20:11:31 +0800

    Use Lomuto partition.

--- 03_algorithm/Sort/QuickSort/quickSort.cpp
+++ 03_algorithm/Sort/QuickSort/quickSort.cpp
@@ -5,32 +5,33 @@
 using namespace std;
 
 int partitionArray(std::vector<int> &v, int start, int end) {
-    int pivot = start;
-    int l = start + 1, r = end;
-
-    while(l <= r) {
-        if(v[l] < v[pivot])
-            l++;
-        else if(v[r] >= v[pivot])
-            r--;
-        else
-            swap(v[l], v[r]);
-    }
-    swap(v[pivot], v[r]);
-    return r; 
+       if (start >= end) return -1;
+      int pivot = end, i = start, j = start;
+      //Swap v[j] and v[i] if v[j] < v[Pivot]. Pivot is the most right element.
+      //j goes through v[]. v[start ~ i-1] < v[Pivot]. Return i which is new pivot.
+      while(j < end){
+        if(v[j] < v[pivot]){
+          swap(v[i], v[j]);
+          i++;
+        }
+        j++;
+      }
+      swap(v[pivot], v[i]);
+      return i;
 }
 
 void quickSort(std::vector<int> &v, int start, int end) {
     if (start >= end) return;
 
     int pivot = partitionArray(v, start, end);
-    quickSort(v, start, pivot);
+    //Divide v and exclude the pivot which is in the correct position.
+    quickSort(v, start, pivot - 1);
     quickSort(v, pivot + 1, end);
 }
 
 int main(void) {
-    //vector<int> v{5, 4, 3, 2, 1};
-    vector<int> v{ 5, 5 };
+    vector<int> v{5, 4, 3, 2, 1, 0};
+    //vector<int> v{ 5, 5 };
     
     quickSort(v, 0, v.size() - 1);
     for (int i : v) {
