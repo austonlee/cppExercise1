9dc8da9ee1bee51956b55d9eec890dbf392e7a9f
AustonLee <auston.cc.lee@mail.foxconn.com>
2023-04-27 23:09:23 +0800

    Modify selection sort with recursive way.

--- 03_algorithm/Sort/SelectionSort/selectionSort.cpp
+++ 03_algorithm/Sort/SelectionSort/selectionSort.cpp
@@ -4,18 +4,28 @@
 
 using namespace std;
 
+void chooseMinimum(std::vector<int> &v, int miniInd, int chkInd, int end) {
+  if(chkInd > end)
+    return;
 
-void selectionSort(std::vector<int> &v) {
-  int size = v.size();
-  //Select minimun value for v[miniInd] (v[0] ~ v[n-2]).
-  for(int miniInd = 0; miniInd < size -1; miniInd++) {
-    //Select minimun value from v[miniInd+1] ~ v[n-1].
-    for(int currentChkInd= miniInd + 1; currentChkInd < size; currentChkInd++) {
-      if(v[currentChkInd] < v[miniInd]) {
-        swap(v[miniInd], v[currentChkInd]);
-      }
-    }
-  }
+  chooseMinimum(v, miniInd, chkInd + 1, end);
+  if(v[chkInd] < v[miniInd])
+    swap(v[miniInd], v[chkInd]);
+}
+
+void selectMinimum(std::vector<int> &v, int miniInd, int end) {
+  //start from miniInd==0.
+  if(miniInd < 0)
+    return;
+
+  //Recursively call until miniInd<0.
+  selectMinimum(v, miniInd - 1, end);
+  //Choose minimum from v[miniInd+1] to v[end]
+  chooseMinimum(v, miniInd, miniInd + 1, end);
+}
+
+void selectionSort(std::vector<int> &v, int start, int end) {
+  selectMinimum(v, end - 1, end);
 
 }
 
@@ -24,7 +34,7 @@ int main(void) {
     //vector<int> v{ 5, 5 };
     vector<int> v{50, 2, 3, 2, 1, 0};
     
-    selectionSort(v);
+    selectionSort(v, 0, v.size() - 1);
     for (int i : v) {
         std::cout << i << " ";
     }
