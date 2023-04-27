7260204b57ab9e58cf4e595447442c9da96e308f
AustonLee <auston.cc.lee@mail.foxconn.com>
2023-04-27 19:31:44 +0800

    Modify insertion sort with recursive way.

--- 03_algorithm/Sort/InsertionSort/insertionSort.cpp
+++ 03_algorithm/Sort/InsertionSort/insertionSort.cpp
@@ -4,27 +4,41 @@
 
 using namespace std;
 
-void insertionSort(std::vector<int> &v) {
-  int unsortedIdx, sortedIdx, key;
-  int size = v.size();
+void recInsert(vector<int> &v, int value){
+  //Correct position found.Insert the 'last' value from recInsertSort().
+  if(v.size() == 0 || v.back() < value)
+    return v.push_back(value);
+  
+  //Keep the last element in 'last'.
+  int last = v.back();
+  v.pop_back();
+  //Recursively find the correct position and insert the 'last' from recInsertSort().
+  recInsert(v, value);
+  //Insert the temporarily kept 'last'.
+  v.push_back(last);
+}
 
-  for(unsortedIdx = 1; unsortedIdx < size; unsortedIdx++){
-    key = v[unsortedIdx];
-    sortedIdx = unsortedIdx - 1;
-    while(sortedIdx >= 0 && key < v[sortedIdx]){
-      v[sortedIdx + 1] = v[sortedIdx];
-      sortedIdx = sortedIdx - 1;
-    }
-    v[sortedIdx + 1] = key;
-  }
+void recInsertionSort(std::vector<int> &v) {
+  //Until 1 element in vector. Start to insert kept last element.
+  if(v.size() <= 1)
+    return;
+
+  //Keep last element in last temporarily.
+  int last = v.back();
+  v.pop_back();
+
+  //Recursively remove last element and keep in 'last'.
+  recInsertionSort(v);
+  //Insert 'last' in order.
+  recInsert(v, last);
 }
 
 int main(void) {
     //vector<int> v{5, 4, 3, 2, 1, 0};
     //vector<int> v{ 5, 5 };
-    vector<int> v{5, 2, 3, 2, 1, 0};
+    vector<int> v{50, 2, 3, 2, 1, 0};
     
-    insertionSort(v);
+    recInsertionSort(v);
     for (int i : v) {
         std::cout << i << " ";
     }
